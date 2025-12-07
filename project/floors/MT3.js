b25eacb56db8da67f6c2019f7d28f95bebabe8c5
roleecorn <roleecorn@gmail.com>
2025-12-07 22:10:29 +0800

    [feat]第三層 樓傳+怪物手冊

--- project/floors/MT3.js
+++ project/floors/MT3.js
@@ -1,43 +1,66 @@
 main.floors.MT3=
 {
-"floorId": "MT3",
-"title": "主塔 3 层",
-"name": "3",
-"width": 13,
-"height": 13,
-"canFlyTo": true,
-"canFlyFrom": true,
-"canUseQuickShop": true,
-"cannotViewMap": true,
-"cannotMoveDirectly": false,
-"images": [],
-"ratio": 1,
-"defaultGround": "",
-"firstArrive": [],
-"eachArrive": [],
-"parallelDo": "",
-"events": {},
-"changeFloor": {},
-"beforeBattle": {},
-"afterBattle": {},
-"afterGetItem": {},
-"afterOpenDoor": {},
-"autoEvent": {},
-"cannotMove": {},
-"cannotMoveIn": {},
-"map": [
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0]
+    "floorId": "MT3",
+    "title": "主塔 3 层",
+    "name": "3",
+    "width": 13,
+    "height": 13,
+    "canFlyTo": true,
+    "canFlyFrom": true,
+    "canUseQuickShop": true,
+    "cannotViewMap": true,
+    "cannotMoveDirectly": false,
+    "images": [],
+    "ratio": 1,
+    "defaultGround": "",
+    "firstArrive": [],
+    "eachArrive": [],
+    "parallelDo": "",
+    "events": {
+        "1,1": [
+            "樓層傳送、怪物圖鑑，沒有這兩個在這座塔裡會很難受",
+            "為了之後的勇者留在這裡",
+            "---------第69勇者留",
+            "\t[弱智,default]這是之前的勇者的留言嗎?",
+            {
+                "type": "animate",
+                "name": "EMsikao"
+            },
+            "\t[弱智,default]真的...很多人進來沒有出去呢"
+        ]
+    },
+    "changeFloor": {
+        "1,11": {
+            "floorId": ":before",
+            "stair": "upFloor"
+        }
+    },
+    "beforeBattle": {},
+    "afterBattle": {},
+    "afterGetItem": {},
+    "afterOpenDoor": {},
+    "autoEvent": {},
+    "cannotMove": {},
+    "cannotMoveIn": {},
+    "map": [
+    [144,144,144,144,144,144,144,144,144,144,144,144,144],
+    [144,129,  2,  0,  2,  0,  2,  0,  2,  0,  2,  0,144],
+    [144, 46,  2,  0,  2,  0,  2,  0,  2,  0,  2,  0,144],
+    [144, 45,  2,  0,  2,  0,  2,  0,  2,  0,  2,  0,144],
+    [144,  0,  2,  0,  2,  0,  2,  0,  2,  0,  2,  0,144],
+    [144,  0,  2,  0,  2,  0,  2,  0,  2,  0,  2,  0,144],
+    [144,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,144],
+    [144,  0,  2,  0,  2,  0,  2,  0,  2,  0,  2,  0,144],
+    [144,  0,  2,  0,  2,  0,  2,  0,  2,  0,  2,  0,144],
+    [144,  0,  2,  0,  2,  0,  2,  0,  2,  0,  2,  0,144],
+    [144,  0,  2,  0,  2,  0,  2,  0,  2,  0,  2,  0,144],
+    [144, 88,  2,  0,  2,  0,  2,  0,  2,  0,  2,  0,144],
+    [144,144,144,144,144,144,144,144,144,144,144,144,144]
 ],
+    "bgmap": [
+
+],
+    "fgmap": [
+
+]
 }
