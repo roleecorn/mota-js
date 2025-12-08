fbf0e573882c4428e5c699e5c02ffb9ae0e21908
roleecorn <roleecorn@gmail.com>
2025-12-08 19:23:40 +0800

    [feat]隨意加幾層地形

--- project/floors/MT5.js
+++ project/floors/MT5.js
@@ -1,43 +1,58 @@
 main.floors.MT5=
 {
-"floorId": "MT5",
-"title": "主塔 5 层",
-"name": "5",
-"width": 13,
-"height": 13,
-"canFlyTo": true,
-"canFlyFrom": true,
-"canUseQuickShop": true,
-"cannotViewMap": true,
-"cannotMoveDirectly": false,
-"images": [],
-"ratio": 1,
-"defaultGround": "",
-"firstArrive": [],
-"eachArrive": [],
-"parallelDo": "",
-"events": {},
-"changeFloor": {},
-"beforeBattle": {},
-"afterBattle": {},
-"afterGetItem": {},
-"afterOpenDoor": {},
-"autoEvent": {},
-"cannotMove": {},
-"cannotMoveIn": {},
-"map": [
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0]
+    "floorId": "MT5",
+    "title": "主塔 5 层",
+    "name": "5",
+    "width": 13,
+    "height": 13,
+    "canFlyTo": true,
+    "canFlyFrom": true,
+    "canUseQuickShop": true,
+    "cannotViewMap": true,
+    "cannotMoveDirectly": false,
+    "images": [],
+    "ratio": 1,
+    "defaultGround": "",
+    "firstArrive": [],
+    "eachArrive": [],
+    "parallelDo": "",
+    "events": {},
+    "changeFloor": {
+        "5,1": {
+            "floorId": ":before",
+            "stair": "upFloor"
+        },
+        "7,1": {
+            "floorId": ":next",
+            "stair": "downFloor"
+        }
+    },
+    "beforeBattle": {},
+    "afterBattle": {},
+    "afterGetItem": {},
+    "afterOpenDoor": {},
+    "autoEvent": {},
+    "cannotMove": {},
+    "cannotMoveIn": {},
+    "map": [
+    [144,144,144,144,144,144,144,144,144,144,144,144,144],
+    [144,  0,  0,  0,  0, 88,  2, 87,  0,  0,  0,  0,144],
+    [144,  0,  2,  2,  2,  2,  2,  2,  2,  2,  2,  0,144],
+    [144,  0,  2,  0,  0,  0,  0,  0,  0,  0,  2,  0,144],
+    [144,  0,  2,  0,  0,  0,  0,  0,  0,  0,  2,  0,144],
+    [144,  0,  2,  0,  0,  0,  0,  0,  0,  0,  2,  0,144],
+    [144,  0,  2,  0,  0,  0,  0,  0,  0,  0,  2,  0,144],
+    [144,  0,  2,  0,  0,  0,  0,  0,  0,  0,  2,  0,144],
+    [144,  0,  2,  0,  0,  0,  0,  0,  0,  0,  2,  0,144],
+    [144,  0,  2,  0,  0,  0,  0,  0,  0,  0,  2,  0,144],
+    [144,  0,  2,  2,  2,  2,  2,  2,  2,  2,  2,  0,144],
+    [144,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,144],
+    [144,144,144,144,144,144,144,144,144,144,144,144,144]
 ],
+    "bgmap": [
+
+],
+    "fgmap": [
+
+]
 }
--- project/floors/MT6.js
+++ project/floors/MT6.js
@@ -1,43 +1,58 @@
 main.floors.MT6=
 {
-"floorId": "MT6",
-"title": "主塔 6 层",
-"name": "6",
-"width": 13,
-"height": 13,
-"canFlyTo": true,
-"canFlyFrom": true,
-"canUseQuickShop": true,
-"cannotViewMap": true,
-"cannotMoveDirectly": false,
-"images": [],
-"ratio": 1,
-"defaultGround": "",
-"firstArrive": [],
-"eachArrive": [],
-"parallelDo": "",
-"events": {},
-"changeFloor": {},
-"beforeBattle": {},
-"afterBattle": {},
-"afterGetItem": {},
-"afterOpenDoor": {},
-"autoEvent": {},
-"cannotMove": {},
-"cannotMoveIn": {},
-"map": [
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
-    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0]
+    "floorId": "MT6",
+    "title": "主塔 6 层",
+    "name": "6",
+    "width": 13,
+    "height": 13,
+    "canFlyTo": true,
+    "canFlyFrom": true,
+    "canUseQuickShop": true,
+    "cannotViewMap": true,
+    "cannotMoveDirectly": false,
+    "images": [],
+    "ratio": 1,
+    "defaultGround": "",
+    "firstArrive": [],
+    "eachArrive": [],
+    "parallelDo": "",
+    "events": {},
+    "changeFloor": {
+        "11,1": {
+            "floorId": ":before",
+            "stair": "upFloor"
+        },
+        "1,1": {
+            "floorId": ":next",
+            "stair": "downFloor"
+        }
+    },
+    "beforeBattle": {},
+    "afterBattle": {},
+    "afterGetItem": {},
+    "afterOpenDoor": {},
+    "autoEvent": {},
+    "cannotMove": {},
+    "cannotMoveIn": {},
+    "map": [
+    [144,144,144,144,144,144,144,144,144,144,144,144,144],
+    [144, 87,  0,  0,  0,  0,  0,  0,  0,  0,  0, 88,144],
+    [144,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,144],
+    [144,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,144],
+    [144,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,144],
+    [144,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,144],
+    [144,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,144],
+    [144,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,144],
+    [144,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,144],
+    [144,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,144],
+    [144,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,144],
+    [144,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,144],
+    [144,144,144,144,144,144,144,144,144,144,144,144,144]
 ],
+    "bgmap": [
+
+],
+    "fgmap": [
+
+]
 }
