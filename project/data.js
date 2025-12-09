bc878724d2d1eb8131476d528915fb52eea4cd07
roleecorn <roleecorn@gmail.com>
2025-12-09 11:29:37 +0800

    [feat]修改樓層排序

--- project/data.js
+++ project/data.js
@@ -20,13 +20,13 @@ var data_a1e2fb4a_e986_4524_b0da_9b7ba7c0874d =
 			"MT8",
 			"MT9",
 			"MT10",
-			"Shop",
-			"MT4_5",
 			"MT11",
 			"MT12",
 			"MT13",
 			"MT14",
-			"MT15"
+			"MT15",
+			"Shop",
+			"MT4_5",
 		],
 		"floorPartitions": [],
 		"images": [
