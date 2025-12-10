8e1f6a696de710c8414e2a1538c196021188e925
roleecorn <roleecorn@gmail.com>
2025-12-10 22:26:31 +0800

    [bug]第三次遇到蓋特的時候如果選擇擱置，整個事件鍊會重跑一次

--- project/floors/MT15.js
+++ project/floors/MT15.js
@@ -47,59 +47,70 @@ main.floors.MT15=
                 "shadow": 0
             },
             "data": [
-                "\t[弱智,default]又是你!這次我不會再眼睜睜的看著你逃跑了汪",
-                "\t[蓋特,Getter.png]狗狗，坐下",
-                "\t[弱智,default](坐下)是的，主人汪",
                 {
-                    "type": "vibrate",
-                    "direction": "horizontal",
-                    "time": 500,
-                    "speed": 5,
-                    "power": 5
-                },
-                "\t[弱智,default](站起來)不對，不對不對不對",
-                "\t[弱智,default]我不能再這樣下去了，我可是勇者國的勇者啊",
-                "\t[蓋特,Getter.png]所以你就說你要不要獎勵",
-                "\t[弱智,default]要的汪，請辱罵我吧汪",
-                "\t[蓋特,Getter.png]好噁心",
-                "\t[弱智,default]謝謝您的獎勵",
-                "\t[弱智,default]所以主人這次是來找我要甚麼的汪",
-                "\t[蓋特,Getter.png]把你身上那瓶聖水給我吧，我有用處",
-                {
-                    "type": "animate",
-                    "name": "EMsikao",
-                    "loc": "hero"
-                },
-                "\t[弱智,default]不要",
-                "\t[蓋特,Getter.png]蛤?",
-                "\t[弱智,default]除非你告訴我你準備拿它來做什麼汪",
-                "\t[蓋特,Getter.png]你確定?",
-                "\t[弱智,default]這...這個東西對我來說很重要，除非你告訴我目的，不然我不會交出來的汪!",
-                {
-                    "type": "animate",
-                    "name": "EMsikao",
-                    "loc": [
-                        5,
-                        7
+                    "type": "if",
+                    "condition": "(!flag:Getter3First)",
+                    "true": [
+                        {
+                            "type": "setValue",
+                            "name": "flag:Getter3First",
+                            "value": "1"
+                        },
+                        "\t[弱智,default]又是你!這次我不會再眼睜睜的看著你逃跑了汪",
+                        "\t[蓋特,Getter.png]狗狗，坐下",
+                        "\t[弱智,default](坐下)是的，主人汪",
+                        {
+                            "type": "vibrate",
+                            "direction": "horizontal",
+                            "time": 500,
+                            "speed": 5,
+                            "power": 5
+                        },
+                        "\t[弱智,default](站起來)不對，不對不對不對",
+                        "\t[弱智,default]我不能再這樣下去了，我可是勇者國的勇者啊",
+                        "\t[蓋特,Getter.png]所以你就說你要不要獎勵",
+                        "\t[弱智,default]要的汪，請辱罵我吧汪",
+                        "\t[蓋特,Getter.png]好噁心",
+                        "\t[弱智,default]謝謝您的獎勵",
+                        "\t[弱智,default]所以主人這次是來找我要甚麼的汪",
+                        "\t[蓋特,Getter.png]把你身上那瓶聖水給我吧，我有用處",
+                        {
+                            "type": "animate",
+                            "name": "EMsikao",
+                            "loc": "hero"
+                        },
+                        "\t[弱智,default]不要",
+                        "\t[蓋特,Getter.png]蛤?",
+                        "\t[弱智,default]除非你告訴我你準備拿它來做什麼汪",
+                        "\t[蓋特,Getter.png]你確定?",
+                        "\t[弱智,default]這...這個東西對我來說很重要，除非你告訴我目的，不然我不會交出來的汪!",
+                        {
+                            "type": "animate",
+                            "name": "EMsikao",
+                            "loc": [
+                                5,
+                                7
+                            ]
+                        },
+                        "\t[蓋特,Getter.png]為了逃出這座塔",
+                        {
+                            "type": "animate",
+                            "name": "SLyiwen",
+                            "loc": "hero",
+                            "async": true
+                        },
+                        "\t[弱智,default]逃出這座塔?",
+                        "\t[蓋特,Getter.png]對，就這樣",
+                        {
+                            "type": "waitAsync"
+                        },
+                        "\t[蓋特,Getter.png]我就站在這裡等了",
+                        {
+                            "type": "animate",
+                            "name": "EMsikao",
+                            "loc": "hero"
+                        }
                     ]
-                },
-                "\t[蓋特,Getter.png]為了逃出這座塔",
-                {
-                    "type": "animate",
-                    "name": "SLyiwen",
-                    "loc": "hero",
-                    "async": true
-                },
-                "\t[弱智,default]逃出這座塔?",
-                "\t[蓋特,Getter.png]對，就這樣",
-                {
-                    "type": "waitAsync"
-                },
-                "\t[蓋特,Getter.png]我就站在這裡等了",
-                {
-                    "type": "animate",
-                    "name": "EMsikao",
-                    "loc": "hero"
                 },
                 {
                     "type": "choices",
@@ -247,5 +258,5 @@ main.floors.MT15=
     "fgmap": [
 
 ],
-"bgm": "bgm.mp3"
+    "bgm": "bgm.mp3"
 }
