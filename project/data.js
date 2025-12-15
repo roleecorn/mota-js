b8dd313be52a5971c5edcd6d40c1d47ef328b9b0
roleecorn <roleecorn@gmail.com>
2025-12-15 23:19:16 +0800

    [feat] 難易度補丁

--- project/data.js
+++ project/data.js
@@ -269,7 +269,7 @@ var data_a1e2fb4a_e986_4524_b0da_9b7ba7c0874d =
 	"firstData": {
 		"title": "最後的勇者之塔",
 		"name": "idiot",
-		"version": "Ver 0.9.1",
+		"version": "Ver 0.9.2",
 		"floorId": "MT0",
 		"hero": {
 			"image": "hero.png",
