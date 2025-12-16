cae2c26e0dec9e2de49dd85f94ced8c8dea141e8
roleecorn <roleecorn@gmail.com>
2025-12-16 10:29:36 +0800

    [feat]商店金額下降

--- project/data.js
+++ project/data.js
@@ -661,14 +661,14 @@ var data_a1e2fb4a_e986_4524_b0da_9b7ba7c0874d =
 		"shops": [
 			{
 				"id": "shop1",
-				"text": "\t[店長,triangle]均一價${Math.min(20+3*flag:shop1,100)}金幣",
+				"text": "\t[店長,triangle]均一價${Math.min(20+3*flag:shop1,80)}金幣",
 				"textInList": "炭烤蜜瓜兔子",
 				"mustEnable": false,
 				"disablePreview": false,
 				"choices": [
 					{
 						"text": "生命+800",
-						"need": "status:money>=20+3*flag:shop1 || status:money>=100",
+						"need": "status:money>=20+3*flag:shop1 || status:money>=80",
 						"action": [
 							{
 								"type": "comment",
@@ -676,13 +676,13 @@ var data_a1e2fb4a_e986_4524_b0da_9b7ba7c0874d =
 							},
 							{
 								"type": "if",
-								"condition": "(20+3*flag:shop1>100)",
+								"condition": "(20+3*flag:shop1>80)",
 								"true": [
 									{
 										"type": "setValue",
 										"name": "status:money",
 										"operator": "-=",
-										"value": "100"
+										"value": "80"
 									}
 								],
 								"false": [
@@ -718,13 +718,13 @@ var data_a1e2fb4a_e986_4524_b0da_9b7ba7c0874d =
 							},
 							{
 								"type": "if",
-								"condition": "(20+3*flag:shop1>100)",
+								"condition": "(20+3*flag:shop1>80)",
 								"true": [
 									{
 										"type": "setValue",
 										"name": "status:money",
 										"operator": "-=",
-										"value": "100"
+										"value": "80"
 									}
 								],
 								"false": [
@@ -760,13 +760,13 @@ var data_a1e2fb4a_e986_4524_b0da_9b7ba7c0874d =
 							},
 							{
 								"type": "if",
-								"condition": "(20+3*flag:shop1>100)",
+								"condition": "(20+3*flag:shop1>80)",
 								"true": [
 									{
 										"type": "setValue",
 										"name": "status:money",
 										"operator": "-=",
-										"value": "100"
+										"value": "80"
 									}
 								],
 								"false": [
