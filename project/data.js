f8b8fcca25559ad6fed03b0c9f869b41f70cfe7a
roleecorn <roleecorn@gmail.com>
2025-12-16 23:52:32 +0800

    [feat]起始點設回Story1

--- project/data.js
+++ project/data.js
@@ -270,7 +270,7 @@ var data_a1e2fb4a_e986_4524_b0da_9b7ba7c0874d =
 		"title": "最後的勇者之塔",
 		"name": "idiot",
 		"version": "Ver 1.0.0",
-		"floorId": "MT0",
+		"floorId": "story_1",
 		"hero": {
 			"image": "hero.png",
 			"animate": false,
